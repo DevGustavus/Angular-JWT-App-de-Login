6e2edc71466fa0b036f35f01cfb389ff6a013a12
Gustavo Machado Pontes <gustavo.pontes@mjv.com.br>
2024-10-23 15:09:30 -0300

    add husky e jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-JWT-App-de-Login');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, Angular-JWT-App-de-Login'
+    );
   });
 });
